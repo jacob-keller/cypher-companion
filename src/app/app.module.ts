e6a515a014108ddfc037ec6d0d992d58f0545e55
Jacob Keller <jacob.keller@gmail.com>
2022-05-18 14:33:28 -0700

    angular: restore routing module
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -13,6 +14,7 @@ import { MainmenuComponent } from './mainmenu/mainmenu.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule
   ],
