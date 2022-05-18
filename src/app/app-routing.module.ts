d5fea65d122c858427864153177b030ec44faf9a
Jacob Keller <jacob.keller@gmail.com>
2022-05-18 15:31:44 -0700

    menu: switch to app-router and add menu collapse
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SummaryComponent } from './character/summary/summary.component';
+import { SkillsComponent } from './character/skills/skills.component';
+import { AbilitiesComponent } from './character/abilities/abilities.component';
+import { CyphersComponent } from './character/cyphers/cyphers.component';
+import { EquipmentComponent } from './character/equipment/equipment.component';
+import { BackgroundComponent } from './character/background/background.component';
+import { NotesComponent } from './character/notes/notes.component';
+
+const routes: Routes = [
+  { path: 'summary', component: SummaryComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'abilities', component: AbilitiesComponent },
+  { path: 'cyphers', component: CyphersComponent },
+  { path: 'equipment', component: EquipmentComponent },
+  { path: 'background', component: BackgroundComponent },
+  { path: 'notes', component: NotesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
