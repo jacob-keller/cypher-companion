a755bb726b9e9b1fe25e1e3829181e1d7e126c4b
Jacob Keller <jacob.keller@gmail.com>
2022-05-19 10:35:27 -0700

    use sessionStorage to save last view
    Instead of storing the last view only during a single page load, store
    it as a sessionStorage object. This way, we can use it even if the user
    manually loads the summary page. Session storage is used so that we
    don't remember this data forever, and only for a single browser session.
    
    With this change, we now remember the last view when expanding the page
    to split the summary and other views. This persists even when the user
    has reloaded the page.
    
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- src/app/last-non-summary-view.service.ts
+++ src/app/last-non-summary-view.service.ts
@@ -1,31 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Router, NavigationEnd, Event } from '@angular/router';
 
+const key = "cypherCompanionLastView";
+
 @Injectable({
   providedIn: 'root'
 })
 export class LastNonSummaryViewService {
-
-  // By default, if we haven't yet navigated anywhere, use /skills
-  //
-  // If the user fully redirects manually to summary, it will potentially do
-  // a fresh page reload. If this occurs, we'll default to skills. Perhaps
-  // we want to keep this in local or session storage?
-  private lastView : string = '/skills';
-
   constructor(private router : Router) {
     router.events.subscribe((event : Event) => {
       if (event instanceof NavigationEnd) {
         const url = event.urlAfterRedirects;
 
         if (url !== '/summary' && url !== '') {
-          this.lastView = url;
+          sessionStorage.setItem(key, url);
         }
       }
     });
   }
 
   getLastView() : string {
-    return this.lastView;
+    const views = [
+      "/skills",
+      "/abilities",
+      "/cyphers",
+      "/equipment",
+      "/background",
+      "/notes"
+    ];
+
+    const lastView = sessionStorage.getItem(key)
+    if (!lastView || !views.includes(lastView)) {
+      // Default to skills if we don't have or don't recognize the view
+      return "/skills";
+    }
+
+    return lastView;
   }
 }
