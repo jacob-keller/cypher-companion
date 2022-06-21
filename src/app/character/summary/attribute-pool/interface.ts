2f8dcbda41d2da05df518c936b28fed1622506d9
Jacob Keller <jacob.keller@gmail.com>
2022-06-21 15:37:33 -0700

    summary: default to 10 points per pool
    Signed-off-by: Jacob Keller <jacob.keller@gmail.com>

--- src/app/character/summary/attribute-pool/interface.ts
+++ src/app/character/summary/attribute-pool/interface.ts
@@ -1,8 +1,8 @@
 /** @format */
 
 export class AttributePool {
-  public current: number = 0;
-  public maximum: number = 0;
+  public current: number = 10;
+  public maximum: number = 10;
   public edge: number = 0;
 
   constructor(public name: string) {}
